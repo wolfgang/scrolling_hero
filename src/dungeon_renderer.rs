393265a566001313e37223e4b58436ff5089833a
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-04-27 19:09:28 +0200

    Move dungeon rendering into struct DungeonRenderer WIP

--- src/dungeon_renderer.rs
+++ src/dungeon_renderer.rs
@@ -1,19 +1,30 @@
 use std::io::{Write};
 
+struct DungeonRenderer {
+}
 
-pub fn render_dungeon(dungeon: &Vec<Vec<u16>>, player_pos: (u32, u32), writer: &mut Write) -> std::io::Result<()> {
-    for (y, row) in dungeon.iter().enumerate() {
-        let mut row_str = String::with_capacity(row.len());
-        for (x, cell) in row.iter().enumerate() {
-            if (x as u32, y as u32) == player_pos { row_str.push('@') }
-            else {
-                if *cell == 0 { row_str.push('.')}
-                if *cell == 1 { row_str.push('#')}                
+impl DungeonRenderer {
+    pub fn new() -> DungeonRenderer {
+        DungeonRenderer {}
+    }
+    pub fn render_dungeon(&self, dungeon: &Vec<Vec<u16>>, player_pos: (u32, u32), writer: &mut Write) -> std::io::Result<()> {
+        for (y, row) in dungeon.iter().enumerate() {
+            let mut row_str = String::with_capacity(row.len());
+            for (x, cell) in row.iter().enumerate() {
+                if (x as u32, y as u32) == player_pos { row_str.push('@') }
+                else {
+                    if *cell == 0 { row_str.push('.')}
+                    if *cell == 1 { row_str.push('#')}                
+                }
             }
+
+            writer.write_fmt(format_args!("{}\n", &row_str))?;
         }
 
-        writer.write_fmt(format_args!("{}\n", &row_str))?;
+        Ok(())
     }
+}
 
-    Ok(())
+pub fn render_dungeon(dungeon: &Vec<Vec<u16>>, player_pos: (u32, u32), writer: &mut Write) -> std::io::Result<()> {
+    DungeonRenderer::new().render_dungeon(dungeon, player_pos, writer)
 }
