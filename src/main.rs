f51e2e00ea13e78460aaf1e1d6b5470ebcabd1d6
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-04-27 16:59:04 +0200

    Use render_dungeon2 in main

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use console::{Term};
-use sch::dungeon_renderer::render_dungeon;
+use sch::dungeon_renderer::render_dungeon2;
 
 fn main() -> std::io::Result<()> {
     let mut term = Term::stdout();
 
     let dungeon = vec![
-        "########.##########",
-        "###.......#########",
-        "######.......######"
+        vec![1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1],
+        vec![1,1,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1],
+        vec![1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1,1,1]
     ];
 
     term.write_line("        @")?;
 
-    render_dungeon(&dungeon, &mut term)?;
+    render_dungeon2(&dungeon, &mut term)?;
 
     Ok(())
 }
