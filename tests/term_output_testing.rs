0b205e14425624e0f80e3fab3442438d738735c2
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-04-27 13:34:09 +0200

    Write multiple times into the Cursor

--- tests/term_output_testing.rs
+++ tests/term_output_testing.rs
@@ -11,12 +11,13 @@ fn can_pass_term_and_cursor_to_same_function() {
     let mut term = Term::stdout();
     write(&mut term, b"HELLO\n").unwrap();
 
-    let mut buff = Cursor::new(vec![]);
+    let mut buff = Cursor::new(Vec::new());
 
-    write(&mut buff, b"HELLO2").unwrap();
+    write(&mut buff, b"HELLO2\n").unwrap();
+    write(&mut buff, b"HELLO3").unwrap();
 
     let reference = buff.get_ref();
-    assert_eq!("HELLO2", str::from_utf8(reference).unwrap());
+    assert_eq!("HELLO2\nHELLO3", str::from_utf8(reference).unwrap());
 }
 
 
