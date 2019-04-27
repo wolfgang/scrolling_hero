9ac1ce11d5c998c60af6da283c8bbd80811f95f9
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-04-27 20:46:02 +0200

    Prototype how to move the player

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
 use std::rc::Rc;
 use std::cell::{RefCell};
-use console::{Term};
+use console::{Key, Term};
 use sch::dungeon_renderer::DungeonRenderer;
 
 fn main() -> std::io::Result<()> {
@@ -17,7 +17,21 @@ fn main() -> std::io::Result<()> {
     let dungeon_renderer = DungeonRenderer::new(&dungeon, &player_pos);
 
     dungeon_renderer.render(&mut term)?;
+
+    loop {
+        let key = term.read_key().unwrap();
+
+        match key {
+            Key::Escape => { return Ok(()) }
+            Key::ArrowLeft => { player_pos.borrow_mut().0 -= 1 }
+            Key::ArrowRight => { player_pos.borrow_mut().0 += 1 }
+            Key::ArrowDown => { player_pos.borrow_mut().1 += 1 }
+            _ => {}
+        }
+
+        term.clear_last_lines(3)?;
+        dungeon_renderer.render(&mut term)?;
+    }
     
-    Ok(())
 }
     
