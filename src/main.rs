abe081bae0644bccd9bbc9bd373e7a127f10ddc2
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-04-27 12:15:03 +0200

    Experiment with passing a Term instance as a Write trait

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use console::{Term};
+use std::io::Write;
 
 fn main() -> std::io::Result<()> {
-    let term = Term::stdout();
-    term.write_line("        @")?;
-    term.write_line("########.##########")?;
-    term.write_line("###.......#########")?;
-    term.write_line("######.......######")?;
+    let mut term = Term::stdout();
+    write(&mut term, b"        @\n")?;
+    write(&mut term, b"########.##########\n")?;
+    write(&mut term, b"###.......#########\n")?;
+    write(&mut term, b"######.......######\n")?;
 
     Ok(())
 }
+
+fn write<W: Write>(writer: &mut W, buf: &[u8]) -> std::io::Result<()> {
+    writer.write(buf)?;
+    Ok(())
+}
+
     
