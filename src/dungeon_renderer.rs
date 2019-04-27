5130050d324c22ab5b4f033581dd02c9a00af046
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-04-27 16:51:58 +0200

    Move function render_dungeon2 to module dungeon_renderer

--- src/dungeon_renderer.rs
+++ src/dungeon_renderer.rs
@@ -8,3 +8,17 @@ pub fn render_dungeon(dungeon: &Vec<&str>, writer: &mut Write) -> std::io::Resul
 
     Ok(())
 }
+
+pub fn render_dungeon2(dungeon: &Vec<Vec<u16>>, writer: &mut Write) -> std::io::Result<()> {
+    for row in dungeon {
+        let mut row_str = String::with_capacity(row.len());
+        for cell in row {
+            if *cell == 0 { row_str.push('.')}
+            if *cell == 1 { row_str.push('#')}
+        }
+
+        writer.write_fmt(format_args!("{}\n", &row_str))?;
+    }
+
+    Ok(())
+}
--- tests/dungeon_renderer_test.rs
+++ tests/dungeon_renderer_test.rs
@@ -1,7 +1,7 @@
-use std::io::{Cursor, Write};
+use std::io::{Cursor};
 use std::str;
 
-use sch::dungeon_renderer::render_dungeon;
+use sch::dungeon_renderer::{render_dungeon, render_dungeon2};
 
 #[test]
 fn renders_dungeon() {
@@ -40,18 +40,4 @@ fn renders_dungeon_from_vectors() {
     assert_eq!(
         "####.####\n##....###\n####.#.##\n", 
         str::from_utf8(reference).unwrap());
-}
-
-fn render_dungeon2(dungeon: &Vec<Vec<u16>>, writer: &mut Write) -> std::io::Result<()> {
-    for row in dungeon {
-        let mut row_str = String::with_capacity(row.len());
-        for cell in row {
-            if *cell == 0 { row_str.push('.')}
-            if *cell == 1 { row_str.push('#')}
-        }
-
-        writer.write_fmt(format_args!("{}\n", &row_str))?;
-    }
-
-    Ok(())
 }
