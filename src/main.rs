daf545c3316053a1e5470cbebb90af583dbe621b
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-04-27 19:52:48 +0200

    Prototype how to render the player on a given position using crossterm_cursor

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::io::Write;
 use console::{Term};
+use crossterm_cursor::{cursor};
+
 use sch::dungeon_renderer::DungeonRenderer;
 
 fn main() -> std::io::Result<()> {
     let mut term = Term::stdout();
+    let mut cursor = cursor();
 
     let dungeon = vec![
         vec![1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1],
@@ -11,8 +15,15 @@ fn main() -> std::io::Result<()> {
     ];
 
     let dungeon_renderer = DungeonRenderer::new(&dungeon);
+
     dungeon_renderer.render(&mut term)?;
 
+    cursor.save_position()?;
+    cursor.move_right(8);
+    cursor.move_up(2);
+    term.write(b"@")?;
+    cursor.reset_position()?;
+    
     Ok(())
 }
     
