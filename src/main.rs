f13d30056850881a37ace31e5a06c98ed66f1eb1
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-04-28 13:23:01 +0200

    Simplify scrolling code

--- src/main.rs
+++ src/main.rs
@@ -30,17 +30,13 @@ fn main() -> std::io::Result<()> {
     let dungeon_renderer = DungeonRenderer::new(&dungeon, &player_pos);
 
     let camera_offset = 2;
-    let mut rendered_lines = camera_offset + 1;
-    let mut visible_lines = 2*camera_offset + 1;
-
-    term.move_cursor_down(rendered_lines)?;
+    let wanted_lines = 2*camera_offset + 1;
 
     loop {
-        term.clear_last_lines(rendered_lines)?;
-        rendered_lines = dungeon_renderer.render(
+        let rendered_lines = dungeon_renderer.render(
             &mut term, 
             player_pos.borrow().1 as i32 - camera_offset as i32, 
-            visible_lines as u32).unwrap();
+            wanted_lines as u32).unwrap();
         
         let key = term.read_key().unwrap();
 
@@ -55,13 +51,11 @@ fn main() -> std::io::Result<()> {
                 if player_pos.borrow().1 < dungeon.len() as u32 - 1 {
                     player_pos.borrow_mut().1 += 1 
                 }
-                else {
-                    visible_lines = camera_offset + 1;
-                }
             }
             _ => {}
         }
 
+        term.clear_last_lines(rendered_lines)?;
     }
     
 }
