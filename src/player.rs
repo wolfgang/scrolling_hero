b53b84cbb049e063ecd4c964445f86878225e6a7
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-01 15:00:53 +0200

    Extract move conditions to a struct PlayerMovePredicate

--- src/player.rs
+++ src/player.rs
@@ -1,15 +1,47 @@
 use std::cell::{Ref, RefCell};
 
+struct PlayerMovePredicate {
+    max_x: u32,
+    max_y: u32,
+}
+
+impl PlayerMovePredicate {
+    pub fn new(max_x: u32, max_y: u32) -> PlayerMovePredicate {
+        PlayerMovePredicate {
+            max_x,
+            max_y,
+        }
+    }
+
+    pub fn can_move_left(&self, mover: &Player) -> bool {
+        mover.position().0 > 0
+    }
+
+    pub fn can_move_right(&self, mover: &Player) -> bool {
+        mover.position().0 < self.max_x
+    }
+
+    pub fn can_move_up(&self, mover: &Player) -> bool {
+        mover.position().1 > 0
+    }
+
+    pub fn can_move_down(&self, mover: &Player) -> bool {
+        mover.position().1 < self.max_y
+    }
+}
+
 pub struct Player {
     position: RefCell<(u32, u32)>,
     max_x: u32,
     max_y: u32,
+    move_predicate: PlayerMovePredicate,
 }
 
 impl Player {
     pub fn new(x: u32, y: u32, max_x: u32, max_y: u32) -> Player {
         Player {
             position: RefCell::new((x, y)),
+            move_predicate: PlayerMovePredicate::new(max_x, max_y),
             max_x,
             max_y,
         }
@@ -20,25 +52,25 @@ impl Player {
     }
 
     pub fn move_left(&self) {
-        if self.position.borrow().0 > 0 {
+        if self.move_predicate.can_move_left(self) {
             self.position.borrow_mut().0 -= 1;
         }
     }
 
     pub fn move_right(&self) {
-        if self.position.borrow().0 < self.max_x {
+        if self.move_predicate.can_move_right(self) {
             self.position.borrow_mut().0 += 1;
         }
     }
 
     pub fn move_up(&self) {
-        if self.position.borrow().1 > 0 {
+        if self.move_predicate.can_move_up(self) {
             self.position.borrow_mut().1 -= 1;
         }
     }
 
     pub fn move_down(&self) {
-        if self.position.borrow().1 < self.max_y {
+        if self.move_predicate.can_move_down(self) {
             self.position.borrow_mut().1 += 1;
         }
     }
