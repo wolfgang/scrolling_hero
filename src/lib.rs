1035244fe1b358f38ca049b1485a4735cf8c640c
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-01 12:32:19 +0200

    Remove old MutablePosition/PlayerController combo

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
 pub mod dungeon_renderer;
 pub mod player;
 pub mod player_controller;
-pub mod player_controller_old;
-pub mod mutable_position;
--- src/mutable_position.rs
+++
@@ -1,8 +0,0 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-
-pub type MutablePosition = Rc<RefCell<(u32, u32)>>;
-
-pub fn new(x: u32, y: u32) -> MutablePosition {
-    Rc::new(RefCell::new((x, y)))
-}
