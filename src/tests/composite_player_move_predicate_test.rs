5620665f25e2b80f3e0fc6e0df1a307c7d4df7e5
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-03 13:26:00 +0200

    Finish testing CompositePlayerMovePredicate

--- src/tests/composite_player_move_predicate_test.rs
+++ src/tests/composite_player_move_predicate_test.rs
@@ -8,6 +8,12 @@ struct MockPredicate {
     result: bool
 }
 
+impl MockPredicate {
+    pub fn new(result: bool) -> MockPredicate {
+        MockPredicate { result }
+    }
+}
+
 impl MovePredicate for MockPredicate {
     fn can_move_left(&self, _mover: &WithPosition) -> bool {
         self.result
@@ -31,10 +37,26 @@ fn always_true_for_empty_composite() {
     assert!(predicate.can_move_right(&player));
 }
 
-//#[test]
-//fn return_value_of_first_failing() {
-//    let predicate = CompositePlayerMovePredicate::new();
-//    let player = player_factory::without_bounds(0, 0);
-//
-//
-//}
+#[test]
+fn returns_false_if_some_are_false() {
+    let mut composite = CompositePlayerMovePredicate::new();
+    let mock1: Rc<MovePredicate> = Rc::new(MockPredicate::new(true));
+    let mock2: Rc<MovePredicate> = Rc::new(MockPredicate::new(false));
+    composite.add(&mock1);
+    composite.add(&mock2);
+    let predicate: Rc<MovePredicate> = Rc::new(composite);
+    let player = Player::new(0, 0, &predicate);
+    assert_eq!(predicate.can_move_right(&player), false);
+}
+
+#[test]
+fn returns_true_if_all_are_true() {
+    let mut composite = CompositePlayerMovePredicate::new();
+    let mock1: Rc<MovePredicate> = Rc::new(MockPredicate::new(true));
+    let mock2: Rc<MovePredicate> = Rc::new(MockPredicate::new(true));
+    composite.add(&mock1);
+    composite.add(&mock2);
+    let predicate: Rc<MovePredicate> = Rc::new(composite);
+    let player = Player::new(0, 0, &predicate);
+    assert_eq!(predicate.can_move_right(&player), true);
+}
