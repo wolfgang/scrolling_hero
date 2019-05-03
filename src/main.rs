c538ab8edcaef7a47ecdbdfc0b363f67bc8040fc
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-03 11:41:39 +0200

    Prototype usage of wall colliding move-predicate

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use core::borrow::Borrow;
 use std::rc::Rc;
 
 use console::Term;
 
 use sch::dungeon_renderer::DungeonRenderer;
 use sch::move_predicate::MovePredicate;
-use sch::player::move_predicates::NonCollidingPlayerMovePredicate;
+use sch::player::move_predicates::{NonCollidingPlayerMovePredicate, WallCollidingPlayerMovePredicate};
 use sch::player::Player;
 use sch::player_controller::PlayerController;
 
@@ -34,9 +35,14 @@ fn main() -> std::io::Result<()> {
     let max_y = dungeon.len() as u32 - 1;
 
     let predicate2: Rc<MovePredicate> = Rc::new(NonCollidingPlayerMovePredicate::new(max_x, max_y));
-    let player2 = Player::new(8, 0, &predicate2);
+    let dungeon_ref = Rc::new(dungeon);
 
-    let dungeon_renderer = DungeonRenderer::new(&dungeon, &player2);
+    let predicate: Rc<MovePredicate> = Rc::new(WallCollidingPlayerMovePredicate::new(&dungeon_ref));
+
+
+    let player2 = Player::new(8, 0, &predicate);
+
+    let dungeon_renderer = DungeonRenderer::new(dungeon_ref.borrow(), &player2);
 
     let player_controller = PlayerController::new(&player2);
 
