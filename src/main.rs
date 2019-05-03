5355cdb45bf8cdbb661cbf65b6f5e669f2eaa1ad
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-03 16:47:23 +0200

    Use Game in main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use console::Term;
+
+use sch::game::Game;
 
 fn main() -> std::io::Result<()> {
-    println!("HELLO");
+    let mut term = Term::stdout();
+
+    let dungeon = vec![
+        vec![1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        vec![1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        vec![1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+        vec![1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+        vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+    ];
+
+    let game = Game::new(dungeon, (8, 0));
+    game.render(&mut term)?;
+
     Ok(())
 }
