2193df009a4fdee8414160f74219a64f401444d0
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-10 09:20:10 +0200

    Use DungeonProvider in main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use console::{Key, Term};
 
+use sch::dungeon_provider::SingleDungeonProvider;
 use sch::game::Game;
 
 fn main() -> std::io::Result<()> {
@@ -20,7 +21,9 @@ fn main() -> std::io::Result<()> {
         vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ];
 
-    let mut game = Game::new(dungeon, (8, 0), 2);
+    let dungeon_provider = SingleDungeonProvider::new(dungeon);
+
+    let mut game = Game::with_dungeon_provider(&dungeon_provider, (8, 0), 2);
 
     loop {
         let num_lines = game.render(&mut term)?;
