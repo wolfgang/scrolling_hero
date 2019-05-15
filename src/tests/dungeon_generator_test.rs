d7375b8bafed214b6a434325cb9d8836d01aeccd
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-15 14:13:20 +0200

    Use property tests to ensure dungeons have mostly different entrance positions

--- src/tests/dungeon_generator_test.rs
+++ src/tests/dungeon_generator_test.rs
@@ -1,12 +1,12 @@
+use proptest::prelude::*;
 use rand::{Rng, thread_rng};
 
-fn generate_dungeon() -> Vec<String> {
+fn generate_dungeon(length: u32) -> Vec<String> {
     let mut rng = thread_rng();
 
-    let length = 10;
     let hole_position = rng.gen_range(1, length - 1);
 
-    let mut result = String::with_capacity(length);
+    let mut result = String::with_capacity(length as usize);
     for i in 0..length {
         if i == hole_position { result.push('.') } else { result.push('#') }
     }
@@ -14,17 +14,32 @@ fn generate_dungeon() -> Vec<String> {
     vec![result]
 }
 
+proptest! {
+    #[test]
+    fn first_line_contains_one_hole(length in 10u32 .. 80) {
+        let dungeon = generate_dungeon(length);
+        prop_assert!(dungeon[0].contains("#.#"));
+        prop_assert_eq!(1, dungeon[0].matches(".").count());
+    }
 
-#[test]
-fn first_line_contains_one_random_hole() {
-    let dungeon = generate_dungeon();
-    assert!(dungeon[0].contains("#.#"));
-    assert_eq!(1, dungeon[0].matches(".").count());
+    #[test]
+    fn different_dungeons_have_mostly_different_entrance_hole_positions(width in 10u32 .. 80) {
+        let dungeon1 = generate_dungeon(width);
+        let dungeon2 = generate_dungeon(width);
+        let dungeon3 = generate_dungeon(width);
+        let dungeon4 = generate_dungeon(width);
+        let dungeon5 = generate_dungeon(width);
+        let hole1 = dungeon1[0].find('.').unwrap();
+
+        let holes = vec![
+            dungeon1[0].find('.').unwrap(),
+            dungeon2[0].find('.').unwrap(),
+            dungeon3[0].find('.').unwrap(),
+            dungeon4[0].find('.').unwrap(),
+            dungeon5[0].find('.').unwrap()
+            ];
+        let iter = holes.into_iter().filter(|x| *x != hole1);
+        prop_assert!(iter.count() > 0);
+    }
 }
 
-#[test]
-fn two_dungeon_have_different_entrance_hole_position() {
-    let dungeon1 = generate_dungeon();
-    let dungeon2 = generate_dungeon();
-    assert_ne!(dungeon1[0].find('.').unwrap(), dungeon2[0].find('.').unwrap());
-}
