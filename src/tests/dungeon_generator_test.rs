670840b0e060750cd161d8eca6d228e8f58ffe19
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-15 14:46:59 +0200

    Generate a second dungeon line, with same floor tile position

--- src/tests/dungeon_generator_test.rs
+++ src/tests/dungeon_generator_test.rs
@@ -4,26 +4,36 @@ use rand::{Rng, thread_rng};
 fn generate_dungeon(length: u32) -> Vec<String> {
     let mut rng = thread_rng();
 
-    let hole_position = rng.gen_range(1, length - 1);
+    let entrance_x = rng.gen_range(1, length - 1);
 
-    let mut result = String::with_capacity(length as usize);
+    let mut line1 = String::with_capacity(length as usize);
     for i in 0..length {
-        if i == hole_position { result.push('.') } else { result.push('#') }
+        if i == entrance_x { line1.push('.') } else { line1.push('#') }
     }
 
-    vec![result]
+    let line2 = line1.clone();
+
+    vec![line1, line2]
+}
+
+#[test]
+fn second_line_contains_consecutive_floor_tiles_under_entrance() {
+    let dungeon = generate_dungeon(16);
+    assert_eq!(2, dungeon.len());
+    let entrance_x = dungeon[0].find('.').unwrap();
+    assert_eq!(Some(entrance_x), dungeon[1].find('.'));
 }
 
 proptest! {
     #[test]
-    fn first_line_contains_one_hole(length in 10u32 .. 80) {
+    fn first_line_contains_one_entrance(length in 10u32 .. 80) {
         let dungeon = generate_dungeon(length);
         prop_assert!(dungeon[0].contains("#.#"));
         prop_assert_eq!(1, dungeon[0].matches(".").count());
     }
 
     #[test]
-    fn different_dungeons_have_mostly_different_entrance_hole_positions(width in 10u32 .. 80) {
+    fn different_dungeons_have_mostly_different_entrance_positions(width in 10u32 .. 80) {
         let dungeon1 = generate_dungeon(width);
         let dungeon2 = generate_dungeon(width);
         let dungeon3 = generate_dungeon(width);
