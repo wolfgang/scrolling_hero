8fd8164a6cd4b7fd0b26be0d794e40b8ef50b369
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-20 20:32:57 +0200

    Tiny steps towards new dungeon generator

---
+++ src/tests/dungeon_generator_test.rs
@@ -0,0 +1,22 @@
+use crate::types::DungeonLayout;
+
+fn generate_dungeon(width: u32) -> DungeonLayout {
+    let mut first_row = Vec::with_capacity(width as usize);
+    first_row.push('#');
+    for _i in 0..width - 2 {
+        first_row.push('.');
+    }
+    first_row.push('#');
+
+
+    vec![first_row]
+}
+
+#[test]
+fn first_row_is_all_floor_except_left_right_walls() {
+    let dungeon1 = generate_dungeon(10);
+    assert_eq!("#........#".chars().collect::<Vec<char>>(), dungeon1[0]);
+
+    let dungeon2 = generate_dungeon(5);
+    assert_eq!("#...#".chars().collect::<Vec<char>>(), dungeon2[0]);
+}
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -2,3 +2,4 @@ pub mod game_test;
 pub mod dungeon_provider_test;
 pub mod how_to_custom_rng_test;
 pub mod dungeon_generator_0_test;
+pub mod dungeon_generator_test;
