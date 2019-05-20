fffbe253466e3e1609cdc1e8d3f1a70a3a0dc184
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-20 21:13:10 +0200

    Sketch carving the path through the dungeon

--- src/tests/dungeon_generator_test.rs
+++ src/tests/dungeon_generator_test.rs
@@ -1,17 +1,7 @@
+use rand::{Rng, thread_rng};
+
 use crate::dungeon_helpers::make_dungeon;
 use crate::types::DungeonLayout;
-
-fn generate_dungeon(width: u32) -> DungeonLayout {
-    let mut first_row = Vec::with_capacity(width as usize);
-    first_row.push('#');
-    for _i in 0..width - 2 {
-        first_row.push('.');
-    }
-    first_row.push('#');
-
-
-    vec![first_row]
-}
 
 fn generate_dungeon_init(width: usize, height: usize) -> DungeonLayout {
     let mut open_row = vec!['.'; width as usize];
@@ -24,13 +14,41 @@ fn generate_dungeon_init(width: usize, height: usize) -> DungeonLayout {
     dungeon
 }
 
-#[test]
-fn first_row_is_all_floor_except_left_right_walls() {
-    let dungeon1 = generate_dungeon(10);
-    assert_eq!(as_chars("#........#"), dungeon1[0]);
+fn generate_dungeon_step(dungeon: &mut DungeonLayout) {
+    // Choose floor tile in first row
+    // Punch floor tile below that
+    // Choose random direction (left, down, right)
+    // Punch floor tile there
+    // Choose random direction except the one we came from
+    // Punch floor tile there
+    // Repeat until we hit an existing floor tile or last row
 
-    let dungeon2 = generate_dungeon(5);
-    assert_eq!(as_chars("#...#"), dungeon2[0]);
+    let width = dungeon[0].len();
+    let height = dungeon.len();
+
+    let mut rng = thread_rng();
+
+    let x = rng.gen_range(2, width - 2);
+    dungeon[1][x] = '.';
+
+    let direction = rng.gen_range(0, 3);
+
+    match direction {
+        0 => {
+            // Left
+            dungeon[1][x - 1] = '.';
+        }
+        1 => {
+            // Down
+            dungeon[2][x] = '.';
+        }
+        2 => {
+            // Right
+            dungeon[1][x + 1] = '.';
+        }
+
+        _ => {}
+    }
 }
 
 #[test]
@@ -46,6 +64,23 @@ fn first_iteration_is_all_walls_except_first_and_last_line() {
         ]),
         dungeon)
 }
+
+#[test]
+fn second_iteration_carves_a_path() {
+    let mut dungeon = generate_dungeon_init(5, 4);
+
+    // Do this with a fixed seed and use first result that looks right as ref
+    generate_dungeon_step(&mut dungeon);
+    assert_eq!(
+        dungeon_from(vec![
+            "#...#",
+            "#..##",
+            "##.##",
+            "#...#",
+        ]),
+        dungeon)
+}
+
 
 fn as_chars(s: &str) -> Vec<char> {
     s.chars().collect()
