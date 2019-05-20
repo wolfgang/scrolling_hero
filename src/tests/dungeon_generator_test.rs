02da0107c56d6ffe9dd08c6a1e1a8e66d0edf592
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-20 20:46:20 +0200

    Progress save - dungeon generation exploration

--- src/tests/dungeon_generator_test.rs
+++ src/tests/dungeon_generator_test.rs
@@ -1,3 +1,4 @@
+use crate::dungeon_helpers::make_dungeon;
 use crate::types::DungeonLayout;
 
 fn generate_dungeon(width: u32) -> DungeonLayout {
@@ -12,6 +13,32 @@ fn generate_dungeon(width: u32) -> DungeonLayout {
     vec![first_row]
 }
 
+fn generate_dungeon_step(width: u32, height: u32) -> DungeonLayout {
+    let mut rows = Vec::with_capacity(height as usize);
+
+    let mut first_row = Vec::with_capacity(width as usize);
+    first_row.push('#');
+    for _i in 0..width - 2 {
+        first_row.push('.');
+    }
+    first_row.push('#');
+
+    let mut middle_row = Vec::with_capacity(width as usize);
+    for _i in 0..width {
+        middle_row.push('#');
+    }
+
+
+    rows.push(first_row.to_vec());
+    for _i in 0..height - 2 {
+        rows.push(middle_row.to_vec());
+    }
+
+    rows.push(first_row.to_vec());
+
+    rows
+}
+
 #[test]
 fn first_row_is_all_floor_except_left_right_walls() {
     let dungeon1 = generate_dungeon(10);
@@ -21,6 +48,19 @@ fn first_row_is_all_floor_except_left_right_walls() {
     assert_eq!(as_chars("#...#"), dungeon2[0]);
 }
 
+#[test]
+fn first_iteration_is_all_walls_except_first_and_last_line() {
+    let dungeon = generate_dungeon_step(5, 4);
+
+    assert_eq!(
+        make_dungeon(vec![
+            "#...#",
+            "#####",
+            "#####",
+            "#...#",
+        ]),
+        (dungeon, (0, 0)))
+}
 
 fn as_chars(s: &str) -> Vec<char> {
     s.chars().collect()
