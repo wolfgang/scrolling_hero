20c7181b21bf6738d7ac7e04066fac377f64a280
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-21 08:18:50 +0200

    Carve a random path into the dungeon, WIP

--- src/tests/dungeon_generator_test.rs
+++ src/tests/dungeon_generator_test.rs
@@ -14,6 +14,10 @@ fn generate_dungeon_init(width: usize, height: usize) -> DungeonLayout {
     dungeon
 }
 
+const LEFT: u8 = 0;
+const RIGHT: u8 = 1;
+const DOWN: u8 = 2;
+
 fn generate_dungeon_step(dungeon: &mut DungeonLayout) {
     // Choose floor tile in first row
     // Punch floor tile below that
@@ -28,26 +32,43 @@ fn generate_dungeon_step(dungeon: &mut DungeonLayout) {
 
     let mut rng = thread_rng();
 
-    let x = rng.gen_range(2, width - 2);
+    let mut x = width / 2;
+    let mut y = 1;
     dungeon[1][x] = '.';
 
-    let direction = rng.gen_range(0, 3);
+    let mut prev_x = x;
+    let mut prev_y = y;
 
-    match direction {
-        0 => {
-            // Left
-            dungeon[1][x - 1] = '.';
-        }
-        1 => {
-            // Down
-            dungeon[2][x] = '.';
-        }
-        2 => {
-            // Right
-            dungeon[1][x + 1] = '.';
+    while y < height - 1 {
+        let mut directions = Vec::new();
+        directions.push(DOWN);
+        if x - 1 > 1 && x - 1 != prev_x { directions.push(LEFT) };
+        if x + 1 < width - 1 && x + 1 != prev_x { directions.push(RIGHT) };
+
+        prev_x = x;
+        prev_y = y;
+
+        let index = rng.gen_range(0, directions.len());
+        let direction = directions[index];
+
+        match direction {
+            LEFT => {
+                x = x - 1;
+            }
+            DOWN => {
+                y = y + 1;
+            }
+            RIGHT => {
+                x = x + 1;
+            }
+
+            _ => {}
         }
 
-        _ => {}
+        dungeon[y][x] = '.';
+
+
+
     }
 }
 
@@ -67,24 +88,21 @@ fn first_iteration_is_all_walls_except_first_and_last_line() {
 
 #[test]
 fn second_iteration_carves_a_path() {
-    let mut dungeon = generate_dungeon_init(5, 4);
+    let mut dungeon = generate_dungeon_init(20, 10);
 
     // Do this with a fixed seed and use first result that looks right as ref
     generate_dungeon_step(&mut dungeon);
-    assert_eq!(
-        dungeon_from(vec![
-            "#...#",
-            "#..##",
-            "##.##",
-            "#...#",
-        ]),
-        dungeon)
+
+    for row in dungeon {
+        println!("{}", row.into_iter().collect::<String>());
+    }
 }
 
 
-fn as_chars(s: &str) -> Vec<char> {
-    s.chars().collect()
-}
+//fn as_chars(s: &str) -> Vec<char> {
+//    s.chars().collect()
+//}
+//
 
 fn dungeon_from(strings: Vec<&str>) -> DungeonLayout {
     let dungeon = make_dungeon(strings);
