1b9bc92c707c6031765b89be9245b1124fd4852b
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-22 09:07:14 +0200

    Clean up and create three reference dungeons to test against

--- src/tests/dungeon_generator_test.rs
+++ src/tests/dungeon_generator_test.rs
@@ -84,11 +84,28 @@ fn first_iteration_is_all_walls_except_first_and_last_line() {
 
 #[test]
 fn second_iteration_carves_a_path() {
-    let mut dungeon1 = generate_dungeon_init(10, 8);
-    generate_dungeon_path(&mut dungeon1, 1000);
+    let dungeon1 = create_dungeon_with_one_path(1000);
+    let dungeon2 = create_dungeon_with_one_path(2000);
+    let dungeon3 = create_dungeon_with_one_path(3000);
 
     let ref_dungeons = seeds_to_dungeons();
+
     assert_eq!(ref_dungeons.get(&1000), Some(&dungeon1));
+    assert_eq!(ref_dungeons.get(&2000), Some(&dungeon2));
+    assert_eq!(ref_dungeons.get(&3000), Some(&dungeon3));
+}
+
+fn create_dungeon_with_one_path(seed: u64) -> DungeonLayout {
+    let mut dungeon = generate_dungeon_init(10, 8);
+    generate_dungeon_path(&mut dungeon, seed);
+    dungeon
+}
+
+#[allow(dead_code)]
+fn print_dungeon_snapshot(dungeon: &DungeonLayout) {
+    for row in dungeon {
+        println!("\"{}\",", row.into_iter().collect::<String>());
+    }
 }
 
 fn dungeon_from(strings: Vec<&str>) -> DungeonLayout {
@@ -108,6 +125,28 @@ fn seeds_to_dungeons() -> HashMap<u64, DungeonLayout> {
         "#######..#",
         "#######..#",
         "#........#"]));
+
+
+    hm.insert(2000, dungeon_from(vec![
+        "#........#",
+        "######..##",
+        "#####..###",
+        "#####..###",
+        "##.....###",
+        "##.....###",
+        "######..##",
+        "#........#"]));
+
+    hm.insert(3000, dungeon_from(vec![
+        "#........#",
+        "###.....##",
+        "#######..#",
+        "###......#",
+        "###.######",
+        "##..######",
+        "##.#######",
+        "#........#"]));
+
 
     hm
 }
