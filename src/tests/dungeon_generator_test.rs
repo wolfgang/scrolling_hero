19d9c4e58a7f2ca99de961049ab05c3698a8f268
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-22 13:54:54 +0200

    Clean up

--- src/tests/dungeon_generator_test.rs
+++ src/tests/dungeon_generator_test.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use rand::rngs::StdRng;
 use rand::SeedableRng;
 
@@ -9,22 +7,47 @@ use crate::types::DungeonLayout;
 
 #[test]
 fn generates_dungeon_with_one_path() {
-    let dungeon1 = create_dungeon_with_one_path(1000);
-    let dungeon2 = create_dungeon_with_one_path(2000);
-    let dungeon3 = create_dungeon_with_one_path(3000);
+    assert_eq!(
+        dungeon_layout(vec![
+            "#........#",
+            "#######..#",
+            "####.....#",
+            "####...###",
+            "######...#",
+            "#######..#",
+            "#######..#",
+            "#........#"]),
+        single_path_dungeon_from_seed(1000));
 
-    let ref_dungeons = seeds_to_dungeons();
+    assert_eq!(
+        dungeon_layout(vec![
+            "#........#",
+            "######..##",
+            "#####..###",
+            "#####..###",
+            "##.....###",
+            "##.....###",
+            "######..##",
+            "#........#"]),
+        single_path_dungeon_from_seed(2000));
 
-    assert_eq!(ref_dungeons.get(&1000), Some(&dungeon1));
-    assert_eq!(ref_dungeons.get(&2000), Some(&dungeon2));
-    assert_eq!(ref_dungeons.get(&3000), Some(&dungeon3));
+    assert_eq!(
+        dungeon_layout(vec![
+            "#........#",
+            "###.....##",
+            "#######..#",
+            "###......#",
+            "###.######",
+            "##..######",
+            "##.#######",
+            "#........#"]),
+        single_path_dungeon_from_seed(3000));
 }
 
-pub fn create_dungeon_with_one_path(seed: u64) -> DungeonLayout {
+pub fn single_path_dungeon_from_seed(seed: u64) -> DungeonLayout {
     let mut rng = StdRng::seed_from_u64(seed);
     dungeon_with_num_paths(1, 10, 8, &mut rng)
 }
-
 
 #[allow(dead_code)]
 fn print_dungeon_snapshot(dungeon: &DungeonLayout) {
@@ -33,43 +56,7 @@ fn print_dungeon_snapshot(dungeon: &DungeonLayout) {
     }
 }
 
-fn dungeon_from(strings: Vec<&str>) -> DungeonLayout {
+fn dungeon_layout(strings: Vec<&str>) -> DungeonLayout {
     let dungeon = make_dungeon(strings);
     dungeon.0
-}
-
-fn seeds_to_dungeons() -> HashMap<u64, DungeonLayout> {
-    let mut hm = HashMap::new();
-
-    hm.insert(1000, dungeon_from(vec![
-        "#........#",
-        "#######..#",
-        "####.....#",
-        "####...###",
-        "######...#",
-        "#######..#",
-        "#######..#",
-        "#........#"]));
-
-
-    hm.insert(2000, dungeon_from(vec![
-        "#........#",
-        "######..##",
-        "#####..###",
-        "#####..###",
-        "##.....###",
-        "##.....###",
-        "######..##",
-        "#........#"]));
-
-    hm.insert(3000, dungeon_from(vec![
-        "#........#",
-        "###.....##",
-        "#######..#",
-        "###......#",
-        "###.######",
-        "##..######",
-        "##.#######",
-        "#........#"]));
-    hm
 }
