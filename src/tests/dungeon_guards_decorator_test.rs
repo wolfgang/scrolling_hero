b87201f8667245d3cf7e94730b1f18d6e81e0e9b
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-27 20:00:37 +0200

    Prototype a basic, random guard generator

--- src/tests/dungeon_guards_decorator_test.rs
+++ src/tests/dungeon_guards_decorator_test.rs
@@ -1,4 +1,34 @@
+use rand::{Rng, SeedableRng};
+use rand::rngs::StdRng;
+
+use crate::tests::dungeon_test_helpers::*;
+use crate::types::DungeonLayout;
+
+fn add_guards(dungeon: &mut DungeonLayout, rng: &mut StdRng) {
+    for y in 0..dungeon.len() {
+        for x in 0..dungeon[y].len() {
+            let guard_roll = rng.gen_range(0, 100);
+            if guard_roll < 20 && dungeon[y][x] == '.' {
+                dungeon[y][x] = 'G';
+            }
+        }
+    }
+}
+
+
 #[test]
 fn generates_guards_randomly_on_floor() {
-    assert_eq!(1, 1);
+    let mut dungeon = dungeon_layout(vec![
+        "#.....#",
+        "###...#",
+        "#...#.#",
+        "#.#...#",
+        "#.....#",
+    ]);
+
+    let mut rng = StdRng::seed_from_u64(1000);
+
+    add_guards(&mut dungeon, &mut rng);
+
+    print_dungeon_snapshot(&dungeon);
 }
