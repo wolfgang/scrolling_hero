90fa4700fda4e09812656325918fbe51bd6eebbf
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-05-29 21:02:13 +0200

    Remove redundant test

--- src/tests/game_combat_test.rs
+++ src/tests/game_combat_test.rs
@@ -3,7 +3,7 @@ use console::Key;
 use crate::tests::game_test_helpers::*;
 
 #[test]
-fn player_collides_with_guard() {
+fn player_kills_guard_after_colliding_twice() {
     let mut game = make_game_with_camera_offset(100, vec![
         "#...#",
         "#G@G#",
@@ -64,44 +64,6 @@ fn player_collides_with_guard() {
         "#...#",
         "#...#",
         "#..@#",
-    ]);
-
-}
-
-
-#[test]
-fn player_kills_guard_after_colliding_twice() {
-    let mut game = make_game(vec![
-        "#...#",
-        "#G@.#",
-        "#...#"
-    ]);
-
-    verify_lines_rendered_start_with(&mut game, vec![
-        "#...#",
-        "#G@.#",
-        "#...#"
-    ]);
-
-    game.on_key(Key::ArrowLeft);
-    verify_lines_rendered_start_with(&mut game, vec![
-        "#...#",
-        "#G@.#",
-        "#...#"
-    ]);
-
-    game.on_key(Key::ArrowLeft);
-    verify_lines_rendered_start_with(&mut game, vec![
-        "#...#",
-        "#@..#",
-        "#...#"
-    ]);
-
-    game.on_key(Key::ArrowRight);
-    verify_lines_rendered_start_with(&mut game, vec![
-        "#...#",
-        "#.@.#",
-        "#...#"
     ]);
 
 }
