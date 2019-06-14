548a7fecd9c4ca6acca38bc6c246d79a5aaffee0
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-06-14 07:57:56 +0200

    Remove obsolete test

--- src/tests/game_state_test.rs
+++ src/tests/game_state_test.rs
@@ -1,18 +1,5 @@
-use std::collections::HashMap;
-
-use crate::combat::Combatant;
 use crate::dungeon_helpers::make_dungeon;
 use crate::game_state::GameState;
-
-#[test]
-fn construct_raw() {
-    GameState {
-        dungeon: vec![],
-        player_position: (0, 0),
-        guards: HashMap::new(),
-        player: Combatant { hp: 0 },
-    };
-}
 
 #[test]
 fn construct_with_new() {
