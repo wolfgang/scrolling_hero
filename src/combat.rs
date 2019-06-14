35a6a67cf0f36e7b589e8c1301bd570a0467d569
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-06-14 07:15:45 +0200

    Rename combat::resolve -> combat::resolve_simple

--- src/combat.rs
+++ src/combat.rs
@@ -2,7 +2,7 @@ pub struct Combatant {
     pub hp: u16
 }
 
-pub fn resolve(player: &mut Combatant, guard: &mut Combatant) {
+pub fn resolve_simple(player: &mut Combatant, guard: &mut Combatant) {
     guard.hp -= 10;
     if guard.hp > 0 { player.hp -= 5; }
 }
