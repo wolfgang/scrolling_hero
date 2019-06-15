82defd5ab4a6977a768317d1ceeb57b1d29e4ef5
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-06-15 14:48:57 +0200

    Fix accidental rename

--- src/_tests/combat_test.rs
+++ src/_tests/combat_test.rs
@@ -5,8 +5,8 @@ use crate::_tests::fixed_dice_roller::FixedDiceRoller;
 use crate::game::combat::Combatant;
 
 #[test]
-fn combat_resolves_1() {
-    let mut player_hits_guard_twice = FixedDiceRoller::new();
+fn player_hits_guard_twice() {
+    let mut dice_roller = FixedDiceRoller::new();
 
     let player = Combatant { hp: 100 };
     let guard_ref = Rc::new(RefCell::new(Combatant { hp: 20 }));
