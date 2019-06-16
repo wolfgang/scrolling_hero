02ddc7df9bcdd7db65328c4cdd35e04530bf2a15
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-06-16 18:47:40 +0200

    Extract functions is_hit and apply_damage in Combatant

--- src/game/combatant.rs
+++ src/game/combatant.rs
@@ -39,12 +39,20 @@ impl Combatant {
     pub fn attack(&self, target: &CombatantRef, dice_roller: &mut DiceRoller) -> u8 {
         let mut damage = 0;
         if self.hp > 0 {
-            let attack_roll = dice_roller.roll(20);
-            if attack_roll + self.attack > target.borrow().defense {
+            let attack_base_roll = dice_roller.roll(20);
+            if target.borrow().is_hit(attack_base_roll + self.attack) {
                 damage = dice_roller.roll(10);
-                target.borrow_mut().hp -= damage as i16;
+                target.borrow_mut().apply_damage(damage)
             }
         }
         damage
+    }
+
+    pub fn is_hit(&self, attack_roll: u8) -> bool {
+        return attack_roll > self.defense
+    }
+
+    pub fn apply_damage(&mut self, damage: u8) {
+        self.hp -= damage as i16;
     }
 }
