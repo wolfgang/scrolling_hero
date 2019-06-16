b4428cdf612c786621f1b7573eec7999ce16dab1
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-06-16 09:35:44 +0200

    Create package for feature tests

---
+++ src/_tests/feature/mod.rs
@@ -0,0 +1,3 @@
+pub mod game_test_helpers;
+pub mod game_test;
+pub mod game_combat_test;
--- src/_tests/mod.rs
+++ src/_tests/mod.rs
@@ -1,8 +1,6 @@
+pub mod feature;
 pub mod rust_test;
 pub mod dungeon_test_helpers;
-pub mod game_test_helpers;
-pub mod game_test;
-pub mod game_combat_test;
 pub mod dungeon_provider_test;
 pub mod dungeon_generator_test;
 pub mod dungeon_guards_decorator_test;
