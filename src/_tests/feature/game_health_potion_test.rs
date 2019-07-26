29372de50d1ba8cf9080b869ba7ad8dff4208390
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-07-26 19:30:40 +0200

    Clean up

--- src/_tests/feature/game_health_potion_test.rs
+++ src/_tests/feature/game_health_potion_test.rs
@@ -5,26 +5,14 @@ use crate::game::{Game, GameConfig};
 
 #[test]
 fn stepping_on_health_potion_displays_new_hp_and_how_much_was_gained() {
-    let config = GameConfig {
-        camera_offset: 100,
-        player_hp: 100,
-        player_defense: 1,
-        guard_hp: 100,
-        // Ensure that the guard always hits
-        guard_attack: 20,
-        ..Default::default()
-    };
-
-    let mut game = make_game_with_config(&config, vec![
+    let mut game = make_game_under_test(vec![
         "#...#",
         "#G@.#",
         "#..H#",
     ]);
 
-
     // Guard attacks a couple of times
     // This is to make sure that we don't randomly heal more than max hp
-
     for _ in 0..10 {
         game.on_key(Key::ArrowLeft);
     }
@@ -32,9 +20,11 @@ fn stepping_on_health_potion_displays_new_hp_and_how_much_was_gained() {
     game.on_key(Key::ArrowDown);
 
     let player_hp_before_heal = game.get_player_hp();
+
     // We consume the health potion
     game.on_key(Key::ArrowRight);
-    // Verify we are healed and the amount is displayed correctly
+
+    // Verify we are healed and the hud is updated correctly
     let player_hp_after_heal = game.get_player_hp();
     assert!(player_hp_after_heal > player_hp_before_heal);
 
@@ -46,7 +36,7 @@ fn stepping_on_health_potion_displays_new_hp_and_how_much_was_gained() {
 
 #[test]
 fn stepping_on_health_potion_removes_it() {
-    let mut game = make_game_with_healthy_player(vec![
+    let mut game = make_game_under_test(vec![
         "#...#",
         "#.@H#",
         "#...#",
@@ -62,11 +52,15 @@ fn stepping_on_health_potion_removes_it() {
     ]);
 }
 
-fn make_game_with_healthy_player(dungeon: Vec<&str>) -> Game {
+fn make_game_under_test(dungeon: Vec<&str>) -> Game {
     make_game_with_config(
         &GameConfig {
             camera_offset: 100,
             player_hp: 100,
+            player_defense: 1,
+            guard_hp: 100,
+            // Ensure that the guard always hits
+            guard_attack: 20,
             ..Default::default()
         },
         dungeon)
